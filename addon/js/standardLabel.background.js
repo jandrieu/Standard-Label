3187f2404a20453b8f54671e429207dfac6769d0
Joe Andrieu <joe@andrieu.net>
2012-08-08 08:47:26 -0700

    fixed misisng comma, simplified comment for google intercept

--- addon/js/standardLabel.background.js
+++ addon/js/standardLabel.background.js
@@ -147,7 +147,7 @@ standardLabelSource.urlIntercepts = [{
          },
 				{
 				hit : /http(s|):\/\/www\.google\.com($|\/$|\/search\?|\/$|\/webhp|\/#)/,
-				comment: "Should capture all google.com search URLs and google.com by itself (which shows up with the trailing /), 20120808 we fixed the regex so it hits on webhp and hash. Passes all tests"
+				comment: "Should capture all google.com search URLs and google.com by itself, and /webhp and /#, but no other google URLs",
 				name : "Google Search",
 				handler: "google_search",
 				id : "google_search", // must be unique
